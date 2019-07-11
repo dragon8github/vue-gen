918129ff7629b75791bc21f78aaa830ae0d7e792
lizhaohong <928532756@qq.com>
2019-07-11 20:50:37 +0800

    + init

---
+++ template.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="<%= name %>">
+        <%= name %>
+    </div>
+</template>
+
+<script>
+export default {
+  name: '<%= name %>',
+  data () {
+    return {
+        items: [],
+        title: 'HelloWorld'
+    }
+  },
+  methods: {
+      go () {
+          console.log('go');
+      }
+  },
+  components: {
+
+  },
+  computed: {
+
+  },
+  watch: {
+
+  },
+  beforeMount () {
+      console.log('<%= name %>');
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "~@/scss/functions.scss";
+.<%= name %> {
+
+}
+</style>
---
+++ vue-gen.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+const path = require('path')
+const fs = require('fs-extra')
+const ejs = require('ejs')
+
+// 获取参数
+const argv = process.argv
+
+// 获取输入文件名
+const files = argv.slice(2)
+
+// 获取命令当前执行路径
+const getCurrentPath = dir => path.join(process.cwd(), dir)
+
+// 获取项目路径
+const getPath = dir => path.join(__dirname, dir)
+
+// 遍历文件
+files.forEach(async (path, key) => {
+    // 获取文件名和后缀
+    const [name, ext = 'vue'] = path.substring(path.lastIndexOf('/') + 1).split('.')
+
+    // 渲染与读取
+    const content = await ejs.renderFile(getPath('./template.vue'), { name }, { async: true })
+
+    // 新建文件
+    await fs.writeFile(getCurrentPath(`${name}.${ext}`), content)
+})
